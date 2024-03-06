a2bb9fddc8d8e96a6696f28dccc884f1a4423a7f
Taek Been Nam <taekbeen93@gmail.com>
2024-03-06 11:21:20 -0500

    add header component

---
+++ components/ui/header/header.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Header from './header';
+
+const meta: Meta<typeof Header> = {
+  title: 'Components/Header',
+  component: Header,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/cURR8cW2EUkTgpHLf5RiVW/NomadHair?type=design&node-id=14-3607&mode=design&t=HCQxtNJl3gSDcBsd-4',
+    },
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {};
