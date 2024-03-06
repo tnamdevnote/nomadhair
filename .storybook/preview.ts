851fb6870dd56a832ad8edf8f21cb523c1fe74ce
Taek Been Nam <taekbeen93@gmail.com>
2024-03-06 14:27:38 -0500

    add more viewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,24 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from "@storybook/react";
+import {
+  INITIAL_VIEWPORTS,
+  MINIMAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
 
-import '../app/globals.css';
+import "../app/globals.css";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
       },
     },
   },
