74e6fadb4c53dc3355fd4bdb86ac8ce84283f4dc
Taek Been Nam <taekbeen93@gmail.com>
2024-03-09 15:06:57 -0500

    about secvtion

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Button from "@/components/ui/button";
 import Image from "next/image";
-import barber from "@/public/illustrations/barber.svg";
+import { barber, departing } from "@/public/illustrations";
 import Container from "@/components/ui/container";
 
 export default function Home() {
   return (
     <>
-      <section className="">
-        <Container className="flex flex-col items-center gap-8 py-8 md:flex-row-reverse md:px-16 lg:px-32">
+      <section aria-label="Hero">
+        <Container className="flex flex-col items-center gap-8 py-12 md:flex-row-reverse md:px-16 md:py-44 lg:px-32">
           <div className="flex w-full flex-1 justify-center">
             <Image src={barber} width={425} alt="barber image" />
           </div>
@@ -25,7 +25,25 @@ export default function Home() {
           </div>
         </Container>
       </section>
-      <section>About</section>
+      <section className="bg-secondary-15">
+        <Container className="flex flex-col items-center gap-8 py-12 md:flex-row md:px-16 md:py-44 lg:px-32">
+          <div className="flex w-full flex-1 justify-center">
+            <Image src={departing} width={425} alt="departing image" />
+          </div>
+          <div className="flex flex-1 flex-col items-center gap-10">
+            <h2 className="text-center text-primary-100 md:text-left lg:text-xl">
+              Your friendly neighborhood hair designer
+            </h2>
+            <p className="text-center md:text-left ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+            <div className="w-full">
+              <Button className="w-full md:w-auto">Learn More</Button>
+            </div>
+          </div>
+        </Container>
+      </section>
       <section>Reviews</section>
     </>
   );
---
+++ public/illustrations/index.ts
@@ -0,0 +1,5 @@
+import barber from "./barber.svg";
+import booking from "./booking.svg";
+import departing from "./departing.svg";
+
+export { barber, booking, departing };
