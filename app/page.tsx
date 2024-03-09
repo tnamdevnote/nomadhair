b5732ddcf82e5f58de7f80f3f88db0af7969ec62
Taek Been Nam <taekbeen93@gmail.com>
2024-03-09 15:25:18 -0500

    add subheading to about section

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
             <Image src={barber} width={425} alt="barber image" />
           </div>
           <div className="flex flex-1 flex-col items-center gap-10">
-            <h2 className="text-center font-medium text-primary-100 md:text-left lg:text-2xl">
+            <h1 className="text-center text-xl font-medium text-primary-100 md:text-left lg:text-2xl">
               Bringing style to your place
-            </h2>
+            </h1>
             <p className="text-center md:text-left ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
               tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
@@ -25,13 +25,16 @@ export default function Home() {
           </div>
         </Container>
       </section>
-      <section className="bg-secondary-15">
+      <section className="bg-secondary-15" aria-labelledby="about">
         <Container className="flex flex-col items-center gap-8 py-12 md:flex-row md:px-16 md:py-44 lg:px-32">
           <div className="flex w-full flex-1 justify-center">
             <Image src={departing} width={425} alt="departing image" />
           </div>
           <div className="flex flex-1 flex-col items-center gap-10">
-            <h2 className="text-center text-primary-100 md:text-left lg:text-xl">
+            <h2 className="text-center text-lg text-primary-100 md:text-left lg:text-xl">
+              <strong id="about" className="mb-4 block text-base font-normal">
+                ABOUT NOMADHAIR
+              </strong>
               Your friendly neighborhood hair designer
             </h2>
             <p className="text-center md:text-left ">
