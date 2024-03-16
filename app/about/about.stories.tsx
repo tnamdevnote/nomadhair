13a03bb502291e7478dad075f3d6f8b6e34f79df
Taek Been Nam <taekbeen93@gmail.com>
2024-03-16 19:57:02 -0400

    add page UI for About route

--- app/about/about.stories.tsx
+++ app/about/about.stories.tsx
@@ -10,7 +10,7 @@ const meta: Meta<typeof About> = {
   parameters: {
     design: {
       type: "figma",
-      url: "https://www.figma.com/file/cURR8cW2EUkTgpHLf5RiVW/NomadHair?type=design&node-id=629-4896&mode=design&t=YvqownauNObC4p76-4",
+      url: "https://www.figma.com/file/cURR8cW2EUkTgpHLf5RiVW/NomadHair?type=design&node-id=664-4897&mode=design&t=YvqownauNObC4p76-4",
     },
     layout: "fullscreen",
   },
