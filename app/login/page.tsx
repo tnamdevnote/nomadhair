fde609b0fe4a2bdce5b093fadd03f269cfc96044
Taek Been Nam <taekbeen93@gmail.com>
2024-03-19 16:08:35 -0400

    login ui

---
+++ app/login/page.tsx
@@ -0,0 +1,30 @@
+import Button from "@/components/atoms/button";
+import { Container } from "@/components/templates/container";
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <Container className="max-w-screen-xs flex min-h-dvh flex-col items-center justify-center">
+      <h1 className="w-full text-left text-primary-100">Login</h1>
+      <section
+        aria-label="login form"
+        className="mt-8 flex w-full flex-col gap-4"
+      >
+        <Button className="w-full" size="lg">
+          Continue with Google
+        </Button>
+        <Button className="w-full" intent="secondary" size="lg">
+          Continue with Facebook
+        </Button>
+        <div className="flex items-center justify-center text-sm">
+          <p>{"Don't have an account?"}</p>
+          <Button variant="link" size="sm" asChild className="font-bold">
+            <Link href="#">Join Now!</Link>
+          </Button>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default LoginPage;
