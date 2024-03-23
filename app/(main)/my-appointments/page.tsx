dad13c26432f8c33d27f16b4d23f5a36169d572a
Taek Been Nam <taekbeen93@gmail.com>
2024-03-22 23:32:44 -0400

    style desktop ui

--- app/(main)/my-appointments/page.tsx
+++ app/(main)/my-appointments/page.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/atoms/button";
+import Separator from "@/components/atoms/separator";
 import { Card, CardContent, CardFooter } from "@/components/molecules/card";
 import { Container, SplitContainer } from "@/components/templates/container";
 import { CalendarIcon, ClockIcon, EditIcon, PlusIcon } from "lucide-react";
@@ -8,7 +9,7 @@ function MyAppointments() {
   const isMobile = /Mob/i.test(navigator.userAgent);
 
   return (
-    <div className="flex flex-col gap-8 py-16 md:pb-32">
+    <div className="flex flex-col gap-8 py-16 md:pb-8">
       <Container className="flex justify-between">
         <h1 className="text-lg font-bold text-primary-100 lg:text-xl">
           My Appointments
@@ -21,8 +22,8 @@ function MyAppointments() {
           New Appointment
         </Button>
       </Container>
-      <SplitContainer className="min-h-dvh">
-        <SplitContainer.Left className="w-full">
+      <SplitContainer>
+        <SplitContainer.Left className="w-full md:max-h-[40rem] md:overflow-auto md:pb-4">
           <section aria-labelledby="upcoming">
             <h2 id="upcoming" className="py-3 text-base font-bold">
               Upcoming
@@ -45,10 +46,18 @@ function MyAppointments() {
                 </p>
               </CardContent>
               <CardFooter className="inline-flex justify-end gap-4">
-                <Button variant="ghost" size="sm" className="flex-1">
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  className="flex-1 md:w-32 md:flex-none"
+                >
                   Cancel
                 </Button>
-                <Button icon={<EditIcon />} size="sm" className="flex-1">
+                <Button
+                  icon={<EditIcon />}
+                  size="sm"
+                  className="flex-1 md:w-32 md:flex-none"
+                >
                   Edit
                 </Button>
               </CardFooter>
@@ -58,23 +67,33 @@ function MyAppointments() {
             <h2 id="past" className="py-3 text-base">
               Past
             </h2>
-            <Card className="bg-neutral-10">
-              <CardContent className="flex flex-col gap-2">
-                <p className="inline-flex gap-2 text-base font-bold">
-                  <CalendarIcon />
-                  Monday, 12 June
-                </p>
-                <p className="inline-flex gap-2 text-base font-bold">
-                  <ClockIcon />
-                  12:30PM
-                </p>
-                <p className="text-sm">3201 Burton Street SE</p>
-              </CardContent>
-            </Card>
+            <div className="flex h-full flex-col gap-4">
+              <Card className="bg-neutral-10">
+                <CardContent className="flex flex-col gap-2">
+                  <p className="inline-flex gap-2 text-base font-bold">
+                    <CalendarIcon />
+                    Monday, 12 June
+                  </p>
+                  <p className="inline-flex gap-2 text-base font-bold">
+                    <ClockIcon />
+                    12:30PM
+                  </p>
+                  <p className="text-sm">3201 Burton Street SE</p>
+                </CardContent>
+              </Card>
+            </div>
           </section>
         </SplitContainer.Left>
-        <SplitContainer.Right className="hidden w-full">
-          Right
+        <SplitContainer.Right className="sticky top-32 hidden w-full md:flex">
+          <Separator
+            orientation="vertical"
+            className="hidden h-auto md:block"
+          />
+          <section aria-labelledby="new-appointment">
+            <h2 id="new-appointment" className="py-3 text-base font-bold">
+              New Appointment
+            </h2>
+          </section>
         </SplitContainer.Right>
       </SplitContainer>
     </div>
