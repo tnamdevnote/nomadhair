c204de5ec600e57dfc3c60b0ebf746eaab39fa67
Taek Been Nam <taekbeen93@gmail.com>
2024-03-26 14:23:46 -0400

    write interaction test case for label

--- components/atoms/label/label.stories.tsx
+++ components/atoms/label/label.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import Label from "./label";
 import { useState } from "react";
 import { within, expect, userEvent } from "@storybook/test";
+import Input from "../input";
 
 const meta: Meta<typeof Label> = {
   title: "Atoms/Label",
@@ -14,4 +15,23 @@ const meta: Meta<typeof Label> = {
 export default meta;
 type Story = StoryObj<typeof Label>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: () => (
+    <fieldset className="flex flex-col gap-2">
+      <Label htmlFor="username">Username</Label>
+      <Input id="username" name="username" placeholder="Enter username" />
+    </fieldset>
+  ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const label = canvas.getByLabelText("Username");
+    const input = canvas.getByRole("textbox");
+
+    expect(label).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+
+    await userEvent.click(label);
+    expect(input).toHaveFocus();
+    await userEvent.tab();
+  },
+};
--- components/atoms/label/label.tsx
+++ components/atoms/label/label.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-);
-
 const Label = React.forwardRef<
   HTMLLabelElement,
-  React.ComponentPropsWithoutRef<"label"> & VariantProps<typeof labelVariants>
+  React.ComponentPropsWithoutRef<"label">
 >(({ className, ...props }, ref) => (
-  <label ref={ref} className={cn(labelVariants(), className)} {...props} />
+  <label
+    ref={ref}
+    className={cn(
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+      className,
+    )}
+    {...props}
+  />
 ));
 Label.displayName = "Label";
 
