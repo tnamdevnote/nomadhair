2690e18cde717f6bf5913b08c5e5c6d4194d2db2
Taek Been Nam <taekbeen93@gmail.com>
2024-03-27 18:52:32 -0400

    add default form story

--- components/molecules/form/form.stories.tsx
+++ components/molecules/form/form.stories.tsx
@@ -14,6 +14,8 @@ import {
 import { useForm } from "react-hook-form";
 import Input from "@/components/atoms/input";
 import Button from "@/components/atoms/button";
+import { Card } from "../card";
+import Separator from "@/components/atoms/separator";
 
 const meta: Meta<typeof Form> = {
   title: "Molecules/Form",
@@ -23,45 +25,82 @@ const meta: Meta<typeof Form> = {
   },
 };
 
-const formSchema = z.object({
-  address1: z.string().min(2).max(50),
-  // address2: z.string().min(2).max(50),
-  // city: z.string().min(2).max(50),
-  // state: z.string().min(2).max(2),
-  // zipCode: z.number().min(5).max(5),
-});
-
 export default meta;
 type Story = StoryObj<typeof Form>;
 
 export const Default: Story = {
   render: () => {
+    // define form validation schema
+    const formSchema = z.object({
+      name: z.string().optional(),
+      email: z.string().optional(),
+    });
+    // Infer types from the formSchema
     const form = useForm<z.infer<typeof formSchema>>({
+      defaultValues: {
+        name: "",
+        email: "",
+      },
+      // Connect our schema to react-hook-form using zodResolver
       resolver: zodResolver(formSchema),
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-      console.log(values);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      try {
+        const res = await new Promise((resolve) => setTimeout(resolve, 3000));
+        if (!res) {
+          throw new Error();
+        }
+        console.log(values);
+      } catch (e) {
+        console.log(e);
+      }
     };
     return (
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="address1"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Address1</FormLabel>
-                <FormControl>
-                  <Input placeholder="Address 1" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+      <Card>
+        <h2 className="text-lg">Contact Info</h2>
+        <Separator />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex w-56 flex-col gap-8 md:w-96"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      error={!!fieldState.error}
+                      placeholder="Enter name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={form.formState.isSubmitting} type="submit">
+              {form.formState.isSubmitting ? "Loading..." : "Submit"}
+            </Button>
+          </form>
+        </Form>
+      </Card>
     );
   },
 };
