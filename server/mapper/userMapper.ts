0ea02964090ec78d86bd3c6fd366a8e906c4143e
hyeonggyujang <gyuhyeonggyu@gmail.com>
2024-03-30 02:35:05 -0400

    resolved merge conflicts

--- server/mapper/userMapper.ts
+++ server/mapper/userMapper.ts
@@ -1,7 +1,7 @@
 import { User, UserType } from "../model/user";
 
 export function mapUser(response: any, userType: UserType) {
-    const result:User = {
+    const result: User = {
         displayName: response.user.displayName,
         email: response.user.email,
         userType: userType
