0c40a1388bef406cc787459739863be56d918670
Taek Been Nam <taekbeen93@gmail.com>
2024-04-01 14:40:48 -0400

    add util for converting datetime

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function dateTimeToUnixTimeStamp(date: string, time: string) {
+  const combinedDateTimeString = date + "T" + time;
+  const dateTime = new Date(combinedDateTimeString);
+  const unixTimeStamp = dateTime.getTime() / 1000;
+  return unixTimeStamp;
+}
+
+export function unixToDateTimeStrings(unixTimeStamp: number) {
+  const dateTime = new Date(unixTimeStamp * 1000);
+  const date = dateTime.toLocaleDateString();
+  const time = dateTime.toLocaleTimeString();
+
+  return {date, time}
+}
--- utils/convertToUnixTimeStamp.ts
+++
@@ -1,6 +0,0 @@
-export function convertToUnixTimeStamp(date: string, time: string) {
-  const combinedDateTimeString = date + "T" + time;
-  const dateTime = new Date(combinedDateTimeString);
-  const unixTimeStamp = dateTime.getTime() / 1000;
-  return unixTimeStamp;
-}
