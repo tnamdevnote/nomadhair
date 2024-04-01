73ede8aff0f3acdf524d2c3bc02c2cc572108912
Taek Been Nam <taekbeen93@gmail.com>
2024-04-01 00:11:03 -0400

    refactor/colocate route api file in appointmentForm

--- components/templates/emailTemplate/emailTemplate.tsx
+++ components/templates/emailTemplate/emailTemplate.tsx
@@ -1,14 +1,17 @@
 interface EmailTemplateProps {
   firstName: string;
-  appointmentDetails: { date: string; time: string; name: string };
+  appointmentDetails: { date: string; time: string; note: string };
 }
 
+/** This is a temporary email template
+ * TODO: use {@link https://react.email/docs/introduction | React Email} to create accessible email template
+ */
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
   appointmentDetails,
 }) => (
   <div>
     <h1>Welcome, {firstName}!</h1>
-    <p>{`Here is your appointment details: ${appointmentDetails.date} ${appointmentDetails.time} ${appointmentDetails.name}`}</p>
+    <p>{`Here is your appointment details: ${appointmentDetails.date} ${appointmentDetails.time} ${appointmentDetails.note}`}</p>
   </div>
 );
