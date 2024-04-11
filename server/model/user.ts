516ef79a2baf383987096d1506807e79dd83f652
Taek Been Nam <taekbeen93@gmail.com>
2024-04-11 10:35:38 -0400

    add comments

--- server/model/user.ts
+++ server/model/user.ts
@@ -1,10 +1,11 @@
 export enum UserType {
-    stylist = 0,
-    customer = 1
+  stylist = 0,
+  customer = 1,
 }
 
 export type User = {
-    email: string,
-    displayName: string,
-    userType: UserType
-}
+  email: string;
+  displayName: string;
+  photoURL: string;
+  userType: UserType;
+};
