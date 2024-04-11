5fd6b8505a2698d6f466695437ade6a3ccae4507
Taek Been Nam <taekbeen93@gmail.com>
2024-04-11 02:20:46 -0400

    make signIn page RSC and add signInButton as RCC

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { Button } from "@/components/atoms/button";
 import { Container } from "@/components/templates/container";
-import { redirect } from "next/navigation";
-import { useAuthContext } from "@/app/authProvider";
+import SignInButton from "./components/signInButton";
 
 const SignIn = () => {
-  const { user, signIn } = useAuthContext();
-
-  if (user) redirect("/");
-
   return (
     <Container className="flex min-h-dvh max-w-screen-sm flex-col items-center justify-center">
       <h1 className="w-full text-left text-xl text-primary-100 lg:text-xl">
@@ -19,17 +11,8 @@ const SignIn = () => {
         aria-label="sign in form"
         className="mt-8 flex w-full flex-col gap-4"
       >
-        <Button className="w-full" size="lg" onClick={() => signIn("Google")}>
-          Continue with Google
-        </Button>
-        <Button
-          className="w-full"
-          intent="secondary"
-          size="lg"
-          onClick={() => signIn("Facebook")}
-        >
-          Continue with Facebook
-        </Button>
+        <SignInButton provider="Google" />
+        <SignInButton provider="Facebook" />
       </section>
     </Container>
   );
