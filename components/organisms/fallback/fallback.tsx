d866afbd225e99e38715a8e853719c37c4d02c7c
Taek Been Nam <taekbeen93@gmail.com>
2024-04-11 21:34:01 -0400

    add not found page in root directory

--- components/organisms/fallback/fallback.tsx
+++ components/organisms/fallback/fallback.tsx
@@ -2,34 +2,29 @@
 
 import React from "react";
 import { Button } from "@/components/atoms/button";
+import Link from "next/link";
 
 export interface FallbackProps {
   image: React.ReactNode;
   title: string;
   body?: string;
   buttonText: string;
-  onButtonClick: () => void;
+  redirect: string;
 }
 
-function Fallback({
-  image,
-  title,
-  body,
-  buttonText,
-  onButtonClick,
-}: FallbackProps) {
+function Fallback({ image, title, body, buttonText, redirect }: FallbackProps) {
   return (
     <section
       aria-label={title}
-      className="flex w-full flex-col items-center gap-16 px-4 py-8 md:px-16 lg:px-32 lg:py-32"
+      className="flex h-screen w-full flex-col items-center justify-center gap-16 px-4 py-8 md:px-16 lg:px-32 lg:py-32"
     >
       {image}
       <div className="flex flex-col items-center">
         <h2 className="text-center text-base">{title}</h2>
         <p className="text-center">{body}</p>
       </div>
-      <Button size="md" onClick={onButtonClick}>
-        {buttonText}
+      <Button size="md" asChild>
+        <Link href={redirect}>{buttonText}</Link>
       </Button>
     </section>
   );
