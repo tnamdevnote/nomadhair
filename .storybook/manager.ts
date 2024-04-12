f2e48b9f0e44f173df895dc48f6cd534e9f48f42
Taek Been Nam <taekbeen93@gmail.com>
2024-04-12 00:12:42 -0400

    add manager.ts file

---
+++ .storybook/manager.ts
@@ -0,0 +1,49 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming";
+
+const theme = create({
+  base: "light",
+
+  // Brand assets
+  brandTitle: "NomadHair",
+  // TODO: Replace with real url.
+  brandUrl: "https://nomadhair-79e6c.web.app/",
+  brandImage:
+    "https://res.cloudinary.com/dtsdpcbcv/image/upload/v1712891768/logo-light-lg_pmi0xn.svg",
+  brandTarget: "_self",
+
+  // Base colors
+  colorSecondary: "#3f3d56",
+
+  // UI
+  appBg: "#f3f3f6",
+  appContentBg: "#FFFFFF",
+  appBorderColor: "#e1e1e6",
+  appBorderRadius: 16,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "#3f3d56",
+  textInverseColor: "#f3f3f6",
+  textMutedColor: "#262534",
+
+  // Toolbar default and active colors
+  barTextColor: "#3f3d56",
+  barSelectedColor: "#3f3d56",
+  barHoverColor: "#585C6D",
+  barBg: "#ffffff",
+
+  // Form colors
+  inputBg: "#ffffff",
+  inputBorder: "#636176",
+  inputTextColor: "#3f3d56",
+  inputBorderRadius: 8,
+});
+
+addons.setConfig({
+  theme,
+  panelPosition: "bottom",
+});
