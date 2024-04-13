28f29bae031f71c69c613b04ab2e548930ce4d2f
Taek Been Nam <taekbeen93@gmail.com>
2024-04-13 15:37:37 -0400

    modify middleware redirect: get cookie from reqeust object

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const currentUser = cookies().get("id")?.value;
+  const currentUser = request.cookies.get("id")?.value;
   console.log(cookies().get("id"));
   if (!currentUser && request.nextUrl.pathname.startsWith("/my-appointments")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
