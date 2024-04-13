d174fdbd1a8e7fefcf142f3649080a1cffecb418
Taek Been Nam <taekbeen93@gmail.com>
2024-04-12 21:21:19 -0400

    rebase

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,18 @@ import {
 } from "firebase/auth";
 
 import { auth } from "@/server/initFirebase";
+import { useRouter } from "next/router";
 
 export function onAuthStateChanged(cb: (user: FirebaseUser | null) => void) {
   return _onAuthStateChanged(auth, (user) => cb(user));
+}
+
+export async function signIn() {}
+
+export async function signOut() {
+  try {
+    auth.signOut();
+  } catch (error) {
+    console.error("Error signing out.", error);
+  }
 }
