1e14cf98229e6f5e228ccb6d992f55ff896620a6
Taek Been Nam <taekbeen93@gmail.com>
2024-04-14 15:40:28 -0400

    enable middleware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get("id")?.value;
   console.log(cookies().get("id"));
-  // if (!currentUser && request.nextUrl.pathname.startsWith("/my-appointments")) {
-  //   return NextResponse.redirect(new URL("/sign-in", request.url));
-  // }
+  if (!currentUser && request.nextUrl.pathname.startsWith("/my-appointments")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 }
 
 // only run middleware on matching path
