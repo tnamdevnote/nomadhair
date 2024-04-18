f2678fc03622d4133437f83f52ed14273bea167e
Taek Been Nam <taekbeen93@gmail.com>
2024-04-18 13:44:05 -0400

    update pageTemplate story

--- archive/pageTemplate/pageTemplate.tsx
+++ archive/pageTemplate/pageTemplate.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/organisms/header";
 import Footer from "@/components/organisms/footer";
 import React from "react";
-import { AuthProvider } from "@/app/authProvider";
 
 export interface PageTemplateProps {
   type?: "default" | "header";
@@ -11,20 +10,20 @@ export interface PageTemplateProps {
 function PageTemplate({ type = "default", children }: PageTemplateProps) {
   if (type === "header") {
     return (
-      <AuthProvider>
-        <Header />
+      <>
+        <Header isAuthenticated={false} />
         <main className="bg-secondary-10">{children}</main>
-      </AuthProvider>
+      </>
     );
   }
   return (
-    <AuthProvider>
-      <Header />
+    <>
+      <Header isAuthenticated={false} />
       <main className="bg-secondary-10 pt-[var(--navbar-height-sm)] lg:pt-[var(--navbar-height-lg)]">
         {children}
       </main>
       <Footer />
-    </AuthProvider>
+    </>
   );
 }
 
