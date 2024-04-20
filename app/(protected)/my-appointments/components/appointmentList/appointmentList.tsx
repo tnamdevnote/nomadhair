d754199ec4fdab869fe8a5a955639e6514049548
Taek Been Nam <taekbeen93@gmail.com>
2024-04-20 14:47:52 -0400

    refactor: server render pastAppointments & add error boundary

--- app/(protected)/my-appointments/components/appointmentList/appointmentList.tsx
+++ app/(protected)/my-appointments/components/appointmentList/appointmentList.tsx
@@ -1,35 +1,8 @@
-import useSWR from "swr";
-
 import { Card, CardContent, CardFooter } from "@/components/molecules/card";
 import { Skeleton } from "@/components/atoms/skeleton";
-import { Error, Fallback } from "@/components/organisms/fallback";
-import Image from "next/image";
-import { booking } from "@/public/illustrations";
-import { Button } from "@/components/atoms/button";
-import { getAppointments } from "@/lib/sanity/client";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Suspense } from "react";
 import { UpcomingAppointments } from "../appointments/upcomingAppointments";
-
-// async function getAppointments() {
-//   const res = await fetch(`/api/my-appointments`);
-//   const responseObj = await res.json();
-
-//   return responseObj;
-// }
-
-const AppointmentPlaceHolder = () => {
-  return (
-    <div className="flex h-52 w-full flex-col items-center justify-center gap-4">
-      <Image
-        src={booking}
-        width={150}
-        alt="book appointment placeholder image"
-      />
-      <p className=" text-primary-100">There are no upcoming appointments.</p>
-    </div>
-  );
-};
+import { PastAppointments } from "../appointments/pastAppointments";
 
 function Loading() {
   return (
@@ -48,62 +21,17 @@ function Loading() {
 }
 
 export async function AppointmentList() {
-  // if (error) {
-  //   return (
-  //     <section
-  //       className="flex h-full w-full items-center justify-center"
-  //       aria-label="something went wrong"
-  //     >
-  //       <Fallback
-  //         className="h-full gap-4 lg:p-16"
-  //         image={<Error className="h-2/3 w-2/3" />}
-  //         title="Oops! Something went wrong."
-  //       >
-  //         <Button onClick={() => mutate("/api/my-appointments")}>
-  //           Try again
-  //         </Button>
-  //       </Fallback>
-  //     </section>
-  //   );
-  // }
-
   return (
     <>
       <section aria-labelledby="upcoming">
         <h2 id="upcoming" className="py-3 text-base font-bold">
           Upcoming
         </h2>
+
         <div className="flex flex-col gap-4">
           <Suspense fallback={<Loading />}>
             <UpcomingAppointments />
           </Suspense>
-          {/* {isLoading ? (
-            <Card>
-              <CardContent className="flex flex-col gap-4">
-                <Skeleton className="h-4 w-1/2" />
-                <Skeleton className="h-4 w-1/2" />
-                <Skeleton className="h-4 w-1/2" />
-              </CardContent>
-              <CardFooter className="inline-flex justify-end gap-4">
-                <Skeleton className="h-8 w-32" />
-                <Skeleton className="h-8 w-32" />
-              </CardFooter>
-            </Card>
-          ) : (
-            <>
-              {data.upcomingAppointments.length === 0 ? (
-                <AppointmentPlaceHolder />
-              ) : (
-                data.upcomingAppointments.map((appointment: Appointment) => (
-                  <AppointmentCard
-                    key={appointment.appointmentId}
-                    type="upcoming"
-                    appointment={appointment}
-                  />
-                ))
-              )}
-            </>
-          )} */}
         </div>
       </section>
       <section aria-labelledby="past" className="mt-8 font-bold">
@@ -111,25 +39,9 @@ export async function AppointmentList() {
           Expired
         </h2>
         <div className="flex h-full flex-col gap-4">
-          {/* {isLoading ? (
-            <Card className="bg-neutral-10">
-              <CardContent className="flex flex-col gap-4">
-                <Skeleton className="h-4 w-1/2" />
-                <Skeleton className="h-4 w-1/2" />
-                <Skeleton className="h-4 w-1/2" />
-              </CardContent>
-            </Card>
-          ) : (
-            <>
-              {data.pastAppointments.map((appointment: Appointment) => (
-                <AppointmentCard
-                  key={appointment.appointmentId}
-                  type="past"
-                  appointment={appointment}
-                />
-              ))}
-            </>
-          )} */}
+          <Suspense fallback={<Loading />}>
+            <PastAppointments />
+          </Suspense>
         </div>
       </section>
     </>
---
+++ app/(protected)/my-appointments/components/appointments/pastAppointments.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent } from "@/components/molecules/card";
+import { CalendarIcon, ClockIcon } from "lucide-react";
+
+import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getAppointments } from "@/lib/sanity/client";
+
+export async function PastAppointments() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const appointments = await getAppointments(user?.id, "past");
+
+  // TODO: add logic for handling error and empty array state.
+  return (
+    <>
+      {appointments.map(
+        ({ id, dateTime, address1, address2, city, state, zipCode }) => (
+          <Card key={id} className="bg-neutral-10 shadow-none">
+            <CardContent className="flex flex-col gap-4">
+              <p className="inline-flex gap-2 text-base font-bold">
+                <CalendarIcon />
+                {new Date(dateTime).toDateString()}
+              </p>
+              <p className="inline-flex gap-2 text-base font-bold">
+                <ClockIcon />
+                {new Date(dateTime).toLocaleTimeString("en-US", {
+                  timeStyle: "short",
+                })}
+              </p>
+              <p className="text-sm">
+                {[address1, address2, city, state, zipCode]
+                  .filter((address) => address)
+                  .join(", ")}
+              </p>
+            </CardContent>
+          </Card>
+        ),
+      )}
+    </>
+  );
+}
