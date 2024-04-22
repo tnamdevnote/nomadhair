287bf45fe3569566bb4f817859c601164a372ea0
Taek Been Nam <taekbeen93@gmail.com>
2024-04-22 13:05:35 -0400

    add select ui

---
+++ components/molecules/select/select.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectItem,
+  SelectValue,
+} from "./select";
+
+const meta: Meta<typeof Select> = {
+  title: "Molecules/Select",
+  component: Select,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <Select>
+        <SelectTrigger aria-label="Hello">
+          <SelectValue placeholder="Hello" />
+        </SelectTrigger>
+        <SelectContent position="popper">
+          <SelectItem value="Profile">Profile</SelectItem>
+          <SelectItem value="My Appointment">My appointments</SelectItem>
+          <SelectItem value="Sign Out">Sign Out</SelectItem>
+        </SelectContent>
+      </Select>
+    );
+  },
+};
