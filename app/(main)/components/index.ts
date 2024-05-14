f27544791e6c9e6a9ed1a5d62cd34409b9200efb
Taek Been Nam <taekbeen93@gmail.com>
2024-05-14 15:06:00 -0400

    remove unused files

--- app/(main)/components/index.ts
+++ app/(main)/components/index.ts
@@ -1,5 +1,5 @@
-import TestimonialSection from "./testimonialSection/testimonialSection";
 import HeroSection from "./heroSection/heroSection";
 import AboutSection from "./aboutSection/aboutSection";
+import FeatureSection from "./featureSection/featureSection";
 
-export { HeroSection, AboutSection, TestimonialSection };
+export { HeroSection, AboutSection, FeatureSection };
